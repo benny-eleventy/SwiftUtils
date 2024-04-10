38eab2d08a0b2125fa5e306881873f792d2958fb
ketanshingade <ketanshingade@gmail.com>
2024-04-10 13:34:20 +0530

    new commit

--- Sources/SwiftUtils/SwiftUtils.swift
+++ Sources/SwiftUtils/SwiftUtils.swift
@@ -1,2 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+
+extension Bundle {
+    public static var swiftUtilsResources: Bundle {
+        return Bundle.module
+    }
+}
