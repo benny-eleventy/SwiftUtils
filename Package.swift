691cf67eb060b58471b5a6029326663323cff062
ketanshingade <ketanshingade@gmail.com>
2024-04-10 14:27:30 +0530

    removed resources

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftUtils",
-            dependencies: [],
-            resources: [.process("Resources")]
+            dependencies: []
+//            resources: [.process("Resources")]
         ),
         .testTarget(
             name: "SwiftUtilsTests",
