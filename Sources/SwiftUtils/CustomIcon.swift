13dcefb08de198df9ba34efe01d68699b598e481
ketanshingade <ketanshingade@gmail.com>
2024-04-30 14:49:16 +0530

    icona added

--- Sources/SwiftUtils/CustomIcon.swift
+++ Sources/SwiftUtils/CustomIcon.swift
@@ -22,18 +22,31 @@ public struct IconStyle {
 public enum Icons: String {
     // System SF Symbols
     case lighteningBolt = "bolt.fill"
- 
+        case star = "star.fill"
+        case heart = "heart.fill"
+        case magnifyingGlass = "magnifyingglass"
+        case envelope = "envelope.fill"
+        case gear = "gear"
+        case trash = "trash.fill"
+        case folder = "folder.fill"
+        case paperplane = "paperplane.fill"
+        case bookmark = "bookmark.fill"
+        case moon = "moon.fill"
+        case sunMax = "sun.max.fill"
+        case pencil = "pencil"
+        case photo = "photo.fill"
+        case camera = "camera.fill"
 
     // Custom icons
     case window = "window"
     case annotation = "annotation"
-    case highestPrority = "highestPriority"
+    case highestPriority = "highestPriority"
     
 
     // Determine if an icon is custom
     private var isCustom: Bool {
         switch self {
-        case .window, .annotation , .highestPrority :
+        case .window, .annotation , .highestPriority :
             return true
         default:
             return false
